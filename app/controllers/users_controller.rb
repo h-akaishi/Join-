0ceff5303f400e9738d1fd7b84bb251174087df0
Hironori Akaishi <ah.gm3622@gmail.com>
2017-10-24 22:52:41 +0900

    facebook login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :user_name?, only: [:show]
 
 
   def show
+    # current_user.user_name?
     @user = User.find_by(user_name: params[:user_name])
   end
 
@@ -14,14 +16,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def new_edit
+  end
+
   def update
-    current_user.update(update_params)
-    flash[:notice] = "プロフィールを編集しました。"
+    if current_user.update(update_params)
+      flash[:notice] = "プロフィールを保存しました。"
+    else
+      flash[:error] = "プロフィールの保存に失敗しました。"
+    end
     redirect_to user_path(current_user.user_name)
   end
 
   private
   def update_params
     params.require(:user).permit(:avatar, :introduction, :affiliation)
+  end
+  # 新規ユーザ（FB）の場合プロフィール編集
+  def user_name?
+    redirect_to  new_edit_user_url(current_user.user_name) if current_user.user_name == "#{current_user.uid}#{current_user.provider}"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,34 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
-  validates :user_name, presence: true, length: { maximun: 16, minimum: 3}, format: { with: /\A[a-z0-9]+\z/i}, uniqueness: true
+  validates :user_name, presence: true, length: { maximun: 30, minimum: 3}, format: { with: /\A[a-z0-9]+\z/i}, uniqueness: true
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>", header_icon: "30x30>", user_icon: "128x128>"}
   validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png","image/gif"]
+
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        user_name: "#{auth.uid}#{auth.provider}",
+        uid:      auth.uid,
+        provider: auth.provider,
+        email:    User.get_email(auth),
+        password: Devise.friendly_token[6, 128],
+        sns_photo: auth.info.image
+      )
+    end
+    user
+  end
+
+  private
+  def self.get_email(auth)
+    email = auth.info.email #ユーザメールアドレスの取得
+    email = "#{auth.uid}-#{auth.provider}@example.com" if email.blank? #取得できなかった場合ダミーメールを生成
+    return email
+  end
+
 end
